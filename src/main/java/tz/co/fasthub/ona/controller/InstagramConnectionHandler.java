e64844ae062ac6990dd734bcd9912df5da9410ad
naamini-fasthub <naamini-fasthub@github.com>
2017-05-05 16:37:37 +0300

    instagram dependency added

---
+++ src/main/java/tz/co/fasthub/ona/controller/InstagramConnectionHandler.java
@@ -0,0 +1,17 @@
+package tz.co.fasthub.ona.controller;
+
+import org.springframework.social.connect.ApiAdapter;
+import org.springframework.social.connect.support.OAuth2ConnectionFactory;
+import org.springframework.social.oauth2.OAuth2ServiceProvider;
+
+/**
+ * Created by Naamini on 5/5/17.
+ */
+public class InstagramConnectionHandler {
+
+
+/*    public InstagramConnectionHandler(String providerId, OAuth2ServiceProvider serviceProvider, ApiAdapter apiAdapter) {
+        super(providerId, serviceProvider, apiAdapter);
+    }
+*/
+}
